4d267d51132c09550cd1d0941362a2d7f42bd013
Robin G <guerin28.robin@gmail.com>
2016-12-30 12:05:56 +0100

    Reajout de RemoveBDD.php

---
+++ app/views/BDDs/Modals/RemoveBDD.php
@@ -0,0 +1,48 @@
+<div class="modal fade" id="myModalRemove">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form action="" method="post" id="formDatabaseRemove">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title">Suppression d'une Base de donnée</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Voulez vous vraiment supprimer la base de donnée <strong id="databaseRemove">{name}</strong></p>
+                </div>
+                <div>
+                    <input type="hidden" name="dbName" value="" id="databaseRemoveInput">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                    <button type="submit" class="btn btn-primary" >Supprimer</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+    $( "#formDatabaseRemove" ).submit(function( event ) {
+        var theDbName = document.getElementById("databaseRemoveInput").value;
+        $.ajax({
+            method: "POST",
+            url: "index.php?p=bdd.delete",
+            data: { dbName: theDbName }
+        }).done(function() {
+            console.log("fait");
+        });
+        alert( "Handler for .submit() called." );
+        event.preventDefault();ok 
+    });
+</script>
+
+
+<script type="text/javascript">
+    function removeBDD(database) {
+        console.log(database);
+        document.getElementById("databaseRemove").innerHTML = database;
+        document.getElementById("databaseRemoveInput").value = database;
+    }
+</script>
