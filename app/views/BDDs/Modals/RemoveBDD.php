02aa82f6f35a097e8730ffa270122b31266c8d9d
Neocell <Neocell@users.noreply.github.com>
2017-01-02 13:57:16 +0100

    lien

--- app/views/BDDs/Modals/RemoveBDD.php
+++ app/views/BDDs/Modals/RemoveBDD.php
@@ -1,7 +1,7 @@
 <div class="modal fade" id="myModalRemove">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <form action="" method="post" id="formDatabaseRemove">
+            <form action="index.php?p=bdd.delete" method="post" id="formDatabaseRemove">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
@@ -16,31 +16,14 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-                    <button type="submit" class="btn btn-primary" >Supprimer</button>
+                    <button type="submit" class="btn btn-primary">Supprimer</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
 
-<script type="text/javascript">
-    $( "#formDatabaseRemove" ).submit(function( event ) {
-        event.preventDefault();
 
-        var theDbName = document.getElementById("databaseRemoveInput").value;
-
-        $.ajax({
-            method: "POST",
-            url: "index.php?p=bdd.delete",
-            data: { dbName: theDbName }
-        }).done(function(data) {
-            // $('#myModalRemove').modal('hide');
-            // location.reload();
-            console.log(data);
-        });
-
-    });
-</script>
 
 
 <script type="text/javascript">
